05c3315696616f185e5dede8a7eadd37fb8e113f
mattiapesce <91664050+mattiapesce@users.noreply.github.com>
2021-11-13 11:52:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-using namespace std;
-
+using namespace std ;
 int main() {
-   return 0;
+    int i=0 , succ=1 , prec=0 , ris=0 , n=0 ;
+    cin >> n ;
+    prec=i ;
+    if(n>=2){
+        cout << succ << endl ;
+        for(int i=2; i<=n; i++ ){
+            ris=(prec+succ) ;
+            prec=succ ;
+            succ=ris ;
+            cout << ris << endl ;
+        }
+    }
+    else {
+        cout << "errore" << endl ;
+    }
+    return 0;
 }
